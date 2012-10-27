d7c51d295e3e1cd01590745b9d667249372efec8
radu <zduci@yahoo.com>
2012-10-27 20:12:22 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Wat::Application.routes.draw do
+  root :to => 'game#index'
+  match '/play' => 'play#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
