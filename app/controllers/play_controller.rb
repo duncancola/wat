67218ff7f9d081b52dd7e9984e3ddaed46f15e6b
radu <zduci@yahoo.com>
2012-10-27 20:44:44 +0100

    Add dummy json response

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,7 +1,17 @@
 class PlayController < ApplicationController
-  def index
-    @questions = {"type" => "lulz"}
 
+  respond_to :json
+  def index
+    @questions = [{
+      :id => 1337, 
+      :type => "text", 
+      :song_url => "", 
+      :options => [{:id => 1, :content => "a"}, 
+                   {:id => 2, :content => "b"},
+                   {:id => 3, :content => "c"}], 
+      :answer => 2,
+      :text => "Choose your destiny",
+      }]
     respond_with(@questions) do |format|
       format.json { render :json => @questions.to_json() }
     end
