4b8cf3c35a00cdb05f6b357b7f64a8a27fdc1ac2
radu <zduci@yahoo.com>
2012-10-28 00:12:11 +0100

    Add seven_digital_rest

--- lib/does_requests.rb
+++ lib/does_requests.rb
@@ -1,11 +1,11 @@
 require 'net/http'
 class DoesRequests
-  def self.get(url)
+  def get(url)
     parsed_url = URI.parse(url)
     Net::HTTP.get_response(parsed_url)
   end
 
-  def self.post(url, args)
+  def post(url, args)
     parsed_url = URI.parse(url)
     Net::HTTP.post_form(parsed_url, args)
   end
