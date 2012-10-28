c3a013568877902cb29b263ac9b78639aee90906
radu <zduci@yahoo.com>
2012-10-28 11:24:20 +0000

    Replace dummy response

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -2,27 +2,10 @@ class PlayController < ApplicationController
 
   respond_to :json
   def index
-    @questions = [{
-      :id => 1337, 
-      :type => "text", 
-      :song_url => "", 
-      :options => [{:id => 1, :content => "a"}, 
-                   {:id => 2, :content => "b"},
-                   {:id => 3, :content => "c"}], 
-      :answer => 2,
-      :text => "Choose your destiny",
-      },
-      {
-		:id => 1338,
-		:type => "text",
-		:song_url => "",
-		:options => [{:id => 1, :content => "yo mama"},
-					 {:id => 2, :content => "shitmuncher"},
-					 {:id => 3, :content => "brian badonde"}
-					],
-		:answer => 3,
-		:text => "Who is awesome?"
-	}]
+    mf = MusicFactory.new(SevenDigitalRest.new(DoesRequests.new))
+    artists = mf.get_artists_single_track("rock")
+    dj = Dj.new
+    @questions = dj.create_questions(artists)
     respond_with(@questions) do |format|
       format.json { render :json => @questions.to_json() }
     end
