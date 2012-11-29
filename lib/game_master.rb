39f441b82e773a1189430be440d2981db52cc8a7
radu <zduci@yahoo.com>
2012-11-29 21:16:17 +0000

    GameMaster retrieves questions

--- lib/game_master.rb
+++ lib/game_master.rb
@@ -4,4 +4,13 @@ class GameMaster
   def self.register_new_game(game_id, genre)
   end
 
+  def self.get_questions(game_id, exclude)
+    all_questions = Game.find_by_uuid(game_id).questions
+    if exclude
+      all_questions.delete_if { |question| exclude.include?(question.number.to_s) }
+    else
+      all_questions
+    end
+  end
+
 end
